059aafed26c16bb89ad26055f4b2c41f510ec93b
Mary Mettille <mary.mettille.brist@gmail.com>
2021-05-28 12:49:39 -0500

    Database set up

--- database.sql
+++ database.sql
@@ -1,12 +1,10 @@
--- database name = to_do_list
-
 CREATE TABLE "to-do" (
 	"id" SERIAL PRIMARY KEY,
 	"taskName" VARCHAR(200) NOT NULL,
-	"completed" BOOLEAN DEFAULT FALSE
+	"isComplete" BOOLEAN DEFAULT FALSE
 );
 
 INSERT INTO "to-do"
-	("taskName", "completed")
+	("taskName", "isComplete")
 VALUES
 	('Make to-do list', 'false');
