e20b2ecab7a8d635610026162694533cf80287fc
Mary Mettille <mary.mettille.brist@gmail.com>
2021-05-28 16:46:05 -0500

    Server Side of Delete Method Complete

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -24,14 +24,31 @@ router.post('/', (req, res) => {
     let values = [req.body.task];
     // ⬇ Inserting the user's input into the database
     pool.query (queryText, values)
-    // ⬇ Sending back a created code to the user
+    // ⬇ Sending back a 'created' code to the user
     .then( result => {
         // ⬇ Checking to see what the result is
         console.log('Response from SQL', result);
         res.sendStatus(201);
         });
-    })
+})
 
+//* DELETE METHOD
+router.delete('/:id', (req, res) => {
+    // ⬇ This will grab the id of the task that we would like to delete
+    const taskToDelete = req.params.id;
+    // ⬇ This tell the database what we'd like to delete and where
+    const queryText = `DELETE FROM "toDo" WHERE "toDo".id = $1;`;
+    // ⬇ Delete sanitized user input from the database
+    pool.query(queryText, [taskToDelete])
+    // ⬇ Sending back a 'ok' code to the user
+    .then( response => {
+        console.log(`You deleted...`, taskToDelete);
+        res.sendStatus(200);
+    }).catch( err => {
+        console.log(`error deleting on server side`);
+        res.sendStatus(500);
+    });
+});
 
 
 
