87e0873698abff7360fead8debcb0a75099f328a
Mary Mettille <mary.mettille.brist@gmail.com>
2021-05-28 11:13:52 -0500

    Set up jQuery, readyNow, clickListeners, and getTasks function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,19 @@
-console.log('testing!');
+$(document).ready(readyNow);
+
+function readyNow(){
+    console.log('jQuery is working, it is safe to manipulate the DOM');
+    // ⬇ Establish click listeners
+    clickListeners();
+    // ⬇ Load existing tasks on page load:
+    getTasks();
+} // end readyNow function
+
+function clickListeners(){
+    console.log('in clickListeners function');
+    //TODO ⬇ click listener for the addBtn
+}
+
+function getTasks(){
+    console.log('in getTasks function');
+    //TODO ⬇ get request
+}
