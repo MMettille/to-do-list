7fd429d39c3a68903753621b09e0e8e839446298
Mary Mettille <mary.mettille.brist@gmail.com>
2021-05-30 12:43:40 -0500

    Bootstrap button for markAsComplete Btn and an SVG danger-outline-btn for the delete

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -151,8 +151,13 @@ function renderTasks(toDo){
             <td>${task.taskName}</td>
             <td>${task.priority}</td>
             <td>${taskStatus}</td>
-            <td><button id="markAsCompleteBtn">Mark As Complete</button></td>
-            <td><button id="deleteBtn">DELETE</button></td>
+            <td><button id="markAsCompleteBtn" class="btn btn-secondary">Mark As Complete</button></td>
+            <td>
+                <button id="deleteBtn" class="btn btn-outline-danger">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-archive-fill" viewBox="0 0 16 16">
+                        <path d="M12.643 15C13.979 15 15 13.845 15 12.5V5H1v7.5C1 13.845 2.021 15 3.357 15h9.286zM5.5 7h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1zM.8 1a.8.8 0 0 0-.8.8V3a.8.8 0 0 0 .8.8h14.4A.8.8 0 0 0 16 3V1.8a.8.8 0 0 0-.8-.8H.8z"/>
+                    </svg>
+                </button></td>
         </tr>
         `);
     }
