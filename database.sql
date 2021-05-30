0e187e1671c19aa46b925ccf2721cc9df079a0bc
Mary Mettille <mary.mettille.brist@gmail.com>
2021-05-30 15:51:57 -0500

    Updated database instructions

--- database.sql
+++ database.sql
@@ -1,10 +1,15 @@
 CREATE TABLE "toDo" (
 	"id" SERIAL PRIMARY KEY,
 	"taskName" VARCHAR(200) NOT NULL,
-	"isComplete" BOOLEAN DEFAULT FALSE
+	"isComplete" BOOLEAN DEFAULT FALSE,
+	"priority" VARCHAR(20)
 );
 
 INSERT INTO "toDo"
-	("taskName", "isComplete")
+	("taskName", "isComplete", "priority")
 VALUES
-	('Make to-do list', 'false');
+	('Make to-do list', 'true', 'NOW'),
+	('Practice Whiteboard Challenges', 'false', 'Soon-ish'),
+	('Prime Feedback Due Everyday!!!', 'false', 'NOW'),
+	('Laundry', 'false', 'Later'),
+	('Revamp Resume', 'false', 'Soon-ish');
